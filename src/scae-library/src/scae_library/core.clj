10948a5902ed6b7303059937cfcd67aff326090d
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2018-02-06 21:16:10 +0000

    [close #7] Create Engine Module

--- src/scae-library/src/scae_library/core.clj
+++ src/scae-library/src/scae_library/core.clj
@@ -1,10 +1,10 @@
-(ns scae-library.core)
+(ns scae-library.core
+  "This namespace is the main 'engine' or entry point of the library, it receives
+  requests and calls the other namespaces to perform the analysis, then returns the
+  result to the caller.")
 
-(defn foo
-  "A dummy function to see if the library can be called from external project
-  \nExpects a map in the form {:code \"\"
-                             :rulebook \"\"}"
+(defn analyse-source-code
+  "Expects a map with the following keys {:code     'string'
+                                          :rulebook 'string'}"
   [request]
-  (str "Value received by SCAE-Library: " request
-       "\nCode section: " (:code request)
-       "\nRulebook section: " (:rulebook request)))
+  (str "Value received by SCAE-Library: " request))
